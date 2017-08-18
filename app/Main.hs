75f69a18fc46af7757515fedae7219c06ae759ab
Roy Blankman <riblankman@gmail.com>
2017-08-17 22:51:57 -0400

    adding css directly to file

--- app/Main.hs
+++ app/Main.hs
@@ -6,11 +6,13 @@
 import           Control.Monad.Fix
 import           Data.Default
 import qualified Data.Text as T
+import           Data.Text.Encoding (encodeUtf8)
 import           Reflex.Dom
 import           HNock
 
 main :: IO ()
-main = mainWidget content
+main = mainWidgetWithCss (encodeUtf8 css) content
+
 
 content :: ( DomBuilder (SpiderTimeline Global) m
            , DomBuilderSpace m ~ GhcjsDomSpace
@@ -83,3 +85,78 @@ nockRedexes = [("?[a b]", "0"),
                ("*[a 10 b c]", "*[a c]"),
                ("", ""),
                ("*a", "*a")]
+
+
+css :: T.Text
+css = " \
+    \html,\
+    \body {\
+    \    margin: 0;\
+    \    padding: 0;\
+    \}\
+    \\
+    \button {\
+    \    margin: 0;\
+    \    padding: 0;\
+    \    border: 0;\
+    \    background: none;\
+    \    font-size: 100%;\
+    \    vertical-align: baseline;\
+    \    font-family: inherit;\
+    \    font-weight: inherit;\
+    \    color: inherit;\
+    \    -webkit-appearance: none;\
+    \    appearance: none;\
+    \    -webkit-font-smoothing: antialiased;\
+    \    -moz-font-smoothing: antialiased;\
+    \    font-smoothing: antialiased;\
+    \}\
+    \\
+    \body {\
+    \    font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;\
+    \    line-height: 1.4em;\
+    \    background: #f5f5f5;\
+    \    color: #4d4d4d;\
+    \    min-width: 230px;\
+    \    max-width: 550px;\
+    \    margin: 0 auto;\
+    \    -webkit-font-smoothing: antialiased;\
+    \    -moz-font-smoothing: antialiased;\
+    \    font-smoothing: antialiased;\
+    \    font-weight: 300;\
+    \}\
+    \.left {\
+    \    float: left;\
+    \    margin-right: 15px;\
+    \}\
+    \/*\
+    \    Hack to remove background from Mobile Safari.\
+    \    Can't use it globally since it destroys checkboxes in Firefox\
+    \*/\
+    \@media screen and (-webkit-min-device-pixel-ratio:0) {\
+    \    .toggle-all,\
+    \    .todo-list li .toggle {\
+    \     background: none;\
+    \    }\
+    \\
+    \    .todo-list li .toggle {\
+    \       height: 40px;\
+    \    }\
+    \\
+    \    .toggle-all {\
+    \       -webkit-transform: rotate(90deg);\
+    \       transform: rotate(90deg);\
+    \       -webkit-appearance: none;\
+    \       appearance: none;\
+    \    }\
+    \}\
+    \\
+    \@media (max-width: 430px) {\
+    \    .footer {\
+    \       height: 50px;\
+    \    }\
+    \\
+    \    .filters {\
+    \       bottom: 10px;\
+    \    }\
+    \}"
